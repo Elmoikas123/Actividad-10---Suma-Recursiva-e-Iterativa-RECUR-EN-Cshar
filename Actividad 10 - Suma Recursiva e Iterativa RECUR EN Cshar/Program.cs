af2634c8533bde8fc48c35bb80c8dfba7ba2cedf
mollo <mollo@PCMOIKAS>
2023-10-29 20:40:47 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 10 - Suma Recursiva e Iterativa RECUR EN Cshar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class Program
+{
+    static int SumaRecursiva(int n)
+    {
+        if (n <= 9)
+        {
+            return n;
+        }
+        else
+        {
+            return SumaRecursiva(n / 10) + n % 10;
+        }
+    }
+
+    static void Main()
+    {
+        int numero = 12345;
+        int resultado = SumaRecursiva(numero);
+        Console.WriteLine($"La suma de los dígitos de {numero} es {resultado}");
+    }
+}
